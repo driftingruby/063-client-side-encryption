ef392bddd01c83d1ee773d8baa4bb72d7f4ccd31
Dave Kimura <dave@k-innovations.net>
2017-01-15 22:49:52 -0500

    063

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,32 +27,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-#     rsa_private_key = "-----BEGIN RSA PRIVATE KEY-----
-# MIICXQIBAAKBgQCnqBHkA9aNdhT7nyHSfbj4hHw125xzLbS06Lpw6MsajVyzc/iV
-# 99rxi/6Zdh6zZ4nINzyOtPjacngWiGEHSb51X7qSNNpTTYB7Ogjd2GJG5IvAvmZj
-# rnX7p2TA9h+jzoJClNr06YH7g2DKtsya5ACUI2k7spHwqlemt2swHAcA0wIDAQAB
-# AoGAHLiLEA+kQqmW4KAbMC7/b/tqN+O+QnDzAwbXYZ5PpOXQmqFkVXSXWkfMPsfg
-# bIVxMg5cXmocM3fmoJ2OWaxFZxkD6LnPs8MqX4yt5WrMQXW3aMvjpQ3PxTgzKizM
-# LzuRqjgZ+4EOxbFaLIacjTGUWkR8DXW6p7a7ySW9Bm2AXzkCQQDZ3OrRROQ1Udlo
-# gdABpvrNlYRIKQsfcJiLbP7lLBIgZdotqAws0GOtLDpJPYjhalNnb0vjQsiG0DCQ
-# mzb2rDjtAkEAxQFBA3xQ0PHwcGz9LWj/kPTbOgJP+YLd5k43IiistNEwM3qbsAg/
-# P1lkeGNc72ACjnS0dfHixAWyc4+B+EGovwJBANRIEH4MtJjHwiG0q2uQurzrPVPa
-# uj3ORrVEZTIEuTzcQbVPE13vR9CLUdffn6qVIeOdbMJqZl4GKewdcXb15RkCQE6E
-# WW0SNwOP6cxlI+LdXj5f2KAeG9HOHrKTqdWrcGoAlb1uuG5IWyzca/avY3LkSSCN
-# 0v7PERofSyhTofFX+fECQQDS7z97iqMUMHJViP0/DbG1ZkE9UYBAZo3eO+vLv/AX
-# zIomfFkAsOiygMVKiwy+ze3qXwIpwhDbkdbm3SQZ2s3E
-# -----END RSA PRIVATE KEY-----
-# "
-
-#     if params[:user] && params[:user][:social_security]
-#       private_key = OpenSSL::PKey::RSA.new(rsa_private_key, Rails.application.secrets.rsa_login_password)
-#       password = params[:user][:social_security]
-#       string = private_key.private_decrypt(Base64.decode64(password))
-#       @user.social_security = string
-#     end
-    
-
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
